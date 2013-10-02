23753867b5c4a40cbefc90fb80bb1f6b99b6e16a
R.A. Lucas <richard@richardalucas.com>
2013-10-01 23:23:14 -0600

    first real commit

--- js/yummy.js
+++ js/yummy.js
@@ -1,27 +1,59 @@
 $(document).ready(function(){
 
-	var cldata = function(JSONobj){
-		var clObj = [];
-		clObj.push(JSONobj);
-		return clObj;
+	var clObj = [];
+
+	var parseTitle = function(obj){
+		var counter = 0;
+		var parseArr = [];
+		for (var key in obj){
+			parseArr[counter] = key.split(' ');
+			counter += 1;
+		}
+		return parseArr;
 	};
 
 	var getSearch = function(searchterm, arr){
+		var fullTitle;
 		for(var i = 0; i < arr.length; i++){
-			//how to search through individual terms from job title; will need to parse the text
+			for(var j = 0; j < arr[i].length; j++){
+				if(searchterm === arr[i][j]){
+					fullTitle = arr[i].join(' ');
+				}
+			}
 		}
+		return fullTitle;
+	};
+
+	var getListing = function(title, obj){
+		var link;
+		for(var key in obj){
+			if(title === key){
+				link = obj[key];
+			}
+		}
+		return link;
 	};
 
 	$('#searchbtn').on('click', function(){
 		var searchTerm = $('#inputTerm').val();
+		console.log(searchTerm);
 
 		var zipCode = $('#inputZipCode').val();
+		console.log(zipCode);
 
+		$.ajax({
+			type: 'GET',
+			url: "http://richardalucas.com/json/results.json",
+			async: false,
+			jsonpCallback: 'jsonCallback',
+			contentType: "application/json",
+			dataType: 'jsonp',success: function(json) {
+				clObj.push(json);
+				var newArr = parseTitle(json);
+				var jobTitle = getSearch(searchTerm, newArr);
+				var jobLink = getListing(jobTitle, json);
+				$(".container").append("<h2>"+jobTitle+"</h2><h3><a href="+jobLink+">"+jobLink+"</a></h3>");
+			},
+		});
 	});
-	
-
-
-	
-
-
 });
